83984ccf8d1f39998d9024fdeb39c12276e02669
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2022-01-05 20:36:23 +0900

    https://github.com/toposoid/toposoid-knowledge-register-web/issues/10

--- test/controllers/HomeControllerSpecEnglish.scala
+++ test/controllers/HomeControllerSpecEnglish.scala
@@ -17,13 +17,14 @@
 package controllers
 
 import com.ideal.linked.data.accessor.neo4j.Neo4JAccessor
+import org.neo4j.driver.Result
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.Play.materializer
 import play.api.libs.json.Json
-import play.api.test._
 import play.api.test.Helpers._
+import play.api.test._
 
 /**
  * Add your spec here.
@@ -31,23 +32,11 @@ import play.api.test.Helpers._
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
-class HomeControllerSpec extends PlaySpec with BeforeAndAfter with BeforeAndAfterAll with GuiceOneAppPerTest with Injecting {
+class HomeControllerSpecEnglish extends PlaySpec with BeforeAndAfter with BeforeAndAfterAll with GuiceOneAppPerTest with Injecting {
 
 
   override def beforeAll(): Unit = {
     Neo4JAccessor.delete()
-  }
-
-  "HomeController POST(japanese knowledge)" should {
-    "returns an appropriate response" in {
-      val controller: HomeController = inject[HomeController]
-      val jsonStr:String = """{"knowledgeList":[{"sentence":"これはテストです。", "lang": "ja_JP", "extentInfoJson":"{}"}]}"""
-      val fr = FakeRequest(POST, "/regist")
-        .withHeaders("Content-type" -> "application/json")
-        .withJsonBody(Json.parse(jsonStr))
-      val result= call(controller.regist(), fr)
-      status(result) mustBe OK
-    }
   }
 
   "HomeController POST(english knowledge)" should {
@@ -59,6 +48,11 @@ class HomeControllerSpec extends PlaySpec with BeforeAndAfter with BeforeAndAfte
         .withJsonBody(Json.parse(jsonStr))
       val result= call(controller.regist(), fr)
       status(result) mustBe OK
+      Thread.sleep(3000)
+      val query = "MATCH x = (:ClaimNode{surface:'This'})-[:ClaimEdge]->(:ClaimNode{surface:'is'})<-[:ClaimEdge]-(:ClaimNode{surface:'Test'})<-[:ClaimEdge]-(:ClaimNode{surface:'a'})　return x"
+      val queryResult:Result = Neo4JAccessor.executeQueryAndReturn(query)
+      assert(queryResult.hasNext())
+
     }
   }
 
